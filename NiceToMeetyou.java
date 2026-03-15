639ecf62ad54c0a4c2455963b74eb9a5ae349274
BinSaleh <66542160+lusker1@users.noreply.github.com>
2026-03-15 23:35:39 +0300

    NiceToMeetyou.java
    asking about your name

---
+++ NiceToMeetyou.java
@@ -0,0 +1,16 @@
+import java.util.* ; 
+
+public class NiceToMeetyou {
+
+  public static void main(String{} xxxx ){
+      Scanner input = new Scanner(System.in) ;
+    
+        System.out.println("What is your name ? : ")
+    String yourName = input.nextLine ; 
+    
+    System.out.println("nice to meet you" + youName + "my name is BinSaleh") ' 
+      
+  }
+
+
+}
