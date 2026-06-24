0a1b2e137f95e11290e891abf4f9d9b86af0159a
BinSaleh <66542160+lusker1@users.noreply.github.com>
2026-06-24 10:51:20 +0300

    While Loop Introduction
    /*
     * ===================================================
     * ســـبحـان الله بحمده ســــبحـان الله الـعـظـيـم
     * ===================================================
     * Author      : BinSaleh (lusker1)
     * Section     : Exam Practice
     * Question    : While Loop Introduction
     * Date        : 2026-06-24
     * Description : Basic implementation of a pre-test loop 
     * to print sequential statements using a counter.
     * ===================================================
     */

---
+++ JAVA/03-Repetition/whileStatement.java
@@ -0,0 +1,28 @@
+/*
+ * ===================================================
+ * ســـبحـان الله بحمده ســــبحـان الله الـعـظـيـم
+ * ===================================================
+ * Author      : BinSaleh (lusker1)
+ * Section     : Exam Practice
+ * Question    : While Loop Introduction
+ * Date        : 2026-06-24
+ * Description : Basic implementation of a pre-test loop 
+ * to print sequential statements using a counter.
+ * ===================================================
+ */
+import java.util.*;
+
+public class whileStatement {
+    
+    public static void main(String [] args) {
+        Scanner input = new Scanner (System.in) ; 
+        int count = 0 ; 
+        
+        while (count <= 100) {
+            System.out.println(count + " Welcome to java");
+                count++; 
+        }
+
+
+    }
+}
