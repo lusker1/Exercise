69a4804e7fe3b63df5ad9697838b1c33a211382f
BinSaleh <66542160+lusker1@users.noreply.github.com>
2026-06-24 11:36:06 +0300

    Dynamic Student Average Calculator
    /*
     * ===================================================
     * ســـبحـان الله بحمده ســــبحـان الله الـعـظـيـم
     * ===================================================
     * Author      : BinSaleh (lusker1)
     * Question    : Dynamic Student Average Calculator
     * Date        : 2026-06-24
     * Description : Calculate the average grade for a dynamic 
     * number of students using a while loop and input handling.
     * ===================================================
     */

---
+++ JAVA/03-Repetition/studentAverage.java
@@ -0,0 +1,41 @@
+
+/*
+ * ===================================================
+ * ســـبحـان الله بحمده ســــبحـان الله الـعـظـيـم
+ * ===================================================
+ * Author      : BinSaleh (lusker1)
+ * Question    : Dynamic Student Average Calculator
+ * Date        : 2026-06-24
+ * Description : Calculate the average grade for a dynamic 
+ * number of students using a while loop and input handling.
+ * ===================================================
+ */
+import java.util.*;
+
+public class studentAverage {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int TOTAL_STUDENT = 0 ; 
+        int student = 1;
+        int grade = 0, sum = 0;
+
+            System.out.println("Enter total students : ");
+            TOTAL_STUDENT = input.nextInt() ; 
+
+        while (student <= TOTAL_STUDENT) {
+
+            System.out.printf("Enter grade for student %d :", student);
+            grade = input.nextInt();
+
+            sum += grade;
+            student++;
+
+        }
+        System.out.println("Average Grade is : " +  ( (double)sum/TOTAL_STUDENT) );
+
+        input.close();
+
+    }
+
+}
