fd354bb0396ab73069dd4d38d3a1e1daf7ef152c
BinSaleh <66542160+lusker1@users.noreply.github.com>
2026-06-27 18:05:31 +0300

    01  - Exercise  Basic For Loop (0 To 10)
    /*
     * ===================================================
     * ســـبحـان الله بحمده ســــبحـان الله الـعـظـيـم
     * ===================================================
     * Author      : BinSaleh (lusker1)
     * Question    : Basic For Loop (0 To 10)
     * Date        : 2026-06-27
     * Description : A simple Java program that uses a for loop 
     * to count and print numbers from 0 up to 10.
     * ===================================================
     */

---
+++ JAVA/03-Repetition/for_loop/CountToTen.java
@@ -0,0 +1,22 @@
+/*
+ * ===================================================
+ * ســـبحـان الله بحمده ســــبحـان الله الـعـظـيـم
+ * ===================================================
+ * Author      : BinSaleh (lusker1)
+ * Question    : Basic For Loop (0 To 10)
+ * Date        : 2026-06-27
+ * Description : A simple Java program that uses a for loop 
+ * to count and print numbers from 0 up to 10.
+ * ===================================================
+ */
+import java.util.*; 
+
+public class CountToTen {
+    public static void main(String[] args) {
+
+        for(int i = 0 ; i <=10  ; i++) {
+
+            System.out.println(i);
+        }
+    }
+}   
