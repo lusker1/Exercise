2550efc3667f6809e2add33bf58c607391a1ff25
BinSaleh <66542160+lusker1@users.noreply.github.com>
2026-06-27 19:08:55 +0300

    03 Exericse - For Loop Counting (2 To 20)
    /*
     * ===================================================
     * ســـبحـان الله بحمده ســــبحـان الله الـعـظـيـم
     * ===================================================
     * Author      : BinSaleh (lusker1)
     * Question    : For Loop Counting (2 To 20)
     * Date        : 2026-06-27
     * Description : A simple Java program that uses a for loop 
     * to count and print numbers from 2 up to 20.
     * ===================================================
     */

---
+++ JAVA/03-Repetition/for_loop/CountToTwenty.java
@@ -0,0 +1,22 @@
+/*
+ * ===================================================
+ * ســـبحـان الله بحمده ســــبحـان الله الـعـظـيـم
+ * ===================================================
+ * Author      : BinSaleh (lusker1)
+ * Question    : For Loop Counting (2 To 20)
+ * Date        : 2026-06-27
+ * Description : A simple Java program that uses a for loop 
+ * to count and print numbers from 2 up to 20.
+ * ===================================================
+ */
+import java.util.*; 
+
+public class CountToTwenty {
+
+        public static void main(String[] args) {
+            
+            for(int i = 2 ; i <= 20 ; i++ ) {
+                System.out.println(i);
+            }
+        }
+}
