a0d047b5ed31f93c24865f74426f4ca8d770a63c
BinSaleh <66542160+lusker1@users.noreply.github.com>
2026-06-27 15:36:33 +0300

    Exercise 02 - 	 Dynamic Student Average Calculator

--- JAVA/03-Repetition/studentAverage.java
+++ JAVA/03-Repetition/studentAverage.java
@@ -13,7 +13,7 @@
 import java.util.*;
 
 public class studentAverage {
-
+ 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int TOTAL_STUDENT = 0 ; 
